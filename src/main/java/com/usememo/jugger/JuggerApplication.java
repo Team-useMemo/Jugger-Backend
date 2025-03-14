e8ff64c81ccc6dd200bd934e9ea11af6d7e7adcc
JjungminLee <julie0964@naver.com>
2025-03-14 20:29:16 +0900

    init: initial commit

---
+++ src/main/java/com/usememo/jugger/JuggerApplication.java
@@ -0,0 +1,13 @@
+package com.usememo.jugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuggerApplication.class, args);
+	}
+
+}
