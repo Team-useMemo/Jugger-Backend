894b06231c2e8716e3e3d5ff91c8e8bef4761645
JjungminLee <julie0964@naver.com>
2025-04-01 23:02:27 +0900

    refactor: ResponseEntity 반환으로 변경

--- src/main/java/com/usememo/jugger/domain/category/controller/CategoryController.java
+++ src/main/java/com/usememo/jugger/domain/category/controller/CategoryController.java
@@ -1,10 +1,10 @@
 package com.usememo.jugger.domain.category.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usememo.jugger.domain.category.dto.PostCategoryDto;
@@ -22,8 +22,10 @@ public class CategoryController {
 	private final CategoryService categoryService;
 
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Mono<Category> createCategory(@RequestBody PostCategoryDto postCategoryDto) {
-		return categoryService.createCategory(postCategoryDto);
+	public Mono<ResponseEntity<Category>> createCategory(@RequestBody PostCategoryDto postCategoryDto) {
+		return categoryService.createCategory(postCategoryDto)
+			.map(savedCategory -> ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(savedCategory));
 	}
 }
