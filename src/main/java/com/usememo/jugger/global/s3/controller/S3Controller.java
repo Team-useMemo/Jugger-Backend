be09e5bce0068ed4e53fc6d3de0ae0ae54d1d059
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-06 16:03:51 +0900

    fix : bucket name을 value 형식으로 변경 #20

--- src/main/java/com/usememo/jugger/global/s3/controller/S3Controller.java
+++ src/main/java/com/usememo/jugger/global/s3/controller/S3Controller.java
@@ -3,16 +3,12 @@ package com.usememo.jugger.global.s3.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.usememo.jugger.global.s3.service.S3Service;
+import com.usememo.jugger.global.s3.service.S3ServiceImplementation;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -21,11 +17,11 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/upload")
 @RequiredArgsConstructor
 public class S3Controller {
-	private final S3Service s3Service;
+	private final S3ServiceImplementation s3ServiceImplementation;
 
 	@PostMapping(value = "/test", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public Mono<ResponseEntity<String>> upload(@RequestPart("file") FilePart file) {
-		return s3Service.uploadFile(file)
+		return s3ServiceImplementation.uploadFile(file)
 			.map(url -> ResponseEntity.ok(url));
 	}
 }
