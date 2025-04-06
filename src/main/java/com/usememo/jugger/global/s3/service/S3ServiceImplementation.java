610c0310eec5964ff63851be575749840b3d8d46
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-06 17:22:44 +0900

    fix : Exception 형식 수정 #20
    Exception 형식을 Enum에 추가해서 동작하도록 수정하였습니다.

--- src/main/java/com/usememo/jugger/global/s3/service/S3ServiceImplementation.java
+++ src/main/java/com/usememo/jugger/global/s3/service/S3ServiceImplementation.java
@@ -1,24 +1,28 @@
 package com.usememo.jugger.global.s3.service;
 
+
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
+
+import com.usememo.jugger.global.exception.s3.S3UploadException;
 
 @Service
 @RequiredArgsConstructor
 public class S3ServiceImplementation implements S3Service{
 
 	private final S3Template s3Template;
-	private static final String BUCKET_NAME = "jugger-bucket";
+	@Value("${spring.cloud.aws.s3.bucket}")
+	private String bucketName;
 
 	public Mono<String> uploadFile(FilePart filePart) {
 		String originalFilename = filePart.filename();
@@ -28,10 +32,10 @@ public class S3ServiceImplementation implements S3Service{
 		return DataBufferUtils.join(filePart.content())
 			.flatMap(dataBuffer -> {
 				try (InputStream inputStream = dataBuffer.asInputStream()) {
-					s3Template.upload(BUCKET_NAME, newFileName, inputStream);
-					return Mono.just("https://" + BUCKET_NAME + ".s3.ap-northeast-2.amazonaws.com/" + newFileName);
-				} catch (IOException e) {
-					return Mono.error(new RuntimeException("파일 업로드 실패", e));
+					s3Template.upload(bucketName, newFileName, inputStream);
+					return Mono.just("https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + newFileName);
+				} catch (Exception e) {
+					return Mono.error(new S3UploadException());
 				}
 			});
 	}
