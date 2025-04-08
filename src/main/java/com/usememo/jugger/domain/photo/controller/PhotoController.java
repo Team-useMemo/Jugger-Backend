8ae3032b7d44e813a259a21357aed08a1f815be4
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-09 00:25:50 +0900

    feat : Get 기능 추가하여 controller 구현  #43

---
+++ src/main/java/com/usememo/jugger/domain/photo/controller/PhotoController.java
@@ -0,0 +1,33 @@
+package com.usememo.jugger.domain.photo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usememo.jugger.domain.photo.dto.GetPhotoDto;
+import com.usememo.jugger.domain.photo.dto.GetPhotoRequestDto;
+import com.usememo.jugger.domain.photo.repository.PhotoRepository;
+import com.usememo.jugger.domain.photo.service.PhotoService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class PhotoController {
+
+	private final PhotoService photoService;
+
+	@GetMapping("/photos")
+	public Flux<GetPhotoDto> getPhotos(
+		@RequestParam String user_uuid,
+		@RequestParam String category_uuid){
+		return photoService.getPhotoDto(GetPhotoRequestDto.builder()
+			.userUuid(user_uuid)
+			.categoryUuid(category_uuid)
+			.build());
+	}
+}
