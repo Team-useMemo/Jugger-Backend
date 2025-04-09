57bbd1d9fecddd4f1397d8bacff4eb65f4f76793
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2025-04-09 21:26:56 +0900

    feat: 일정조회 API 개발 (#44)

---
+++ src/main/java/com/usememo/jugger/domain/calendar/dto/GetCalendarDto.java
@@ -0,0 +1,17 @@
+package com.usememo.jugger.domain.calendar.dto;
+
+import java.time.Instant;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class GetCalendarDto {
+	private Instant startDateTime;
+	private Instant endDateTime;
+
+	private String categoryId;
+	private String categoryColor;
+	private String title;
+}
--- src/main/java/com/usememo/jugger/domain/calendar/service/CalendarServiceImplementation.java
+++ src/main/java/com/usememo/jugger/domain/calendar/service/CalendarServiceImplementation.java
@@ -1,16 +1,20 @@
 package com.usememo.jugger.domain.calendar.service;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.usememo.jugger.domain.calendar.dto.GetCalendarDto;
 import com.usememo.jugger.domain.calendar.dto.PostCalendarDto;
 import com.usememo.jugger.domain.calendar.entity.Calendar;
 import com.usememo.jugger.domain.calendar.repository.CalendarRepository;
+import com.usememo.jugger.domain.category.repository.CategoryRepository;
 import com.usememo.jugger.domain.chat.entity.Chat;
 import com.usememo.jugger.domain.chat.repository.ChatRepository;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -19,6 +23,8 @@ public class CalendarServiceImplementation implements CalendarService {
 
 	private final CalendarRepository calendarRepository;
 	private final ChatRepository chatRepository;
+
+	private final CategoryRepository categoryRepository;
 
 	@Override
 	public Mono<Calendar> postCalendar(PostCalendarDto postCalendarDto) {
@@ -44,6 +50,22 @@ public class CalendarServiceImplementation implements CalendarService {
 		return calendarRepository.save(calendar)
 			.flatMap(savedCalendar ->
 				chatRepository.save(chat).thenReturn(savedCalendar)
+			);
+	}
+
+	@Override
+	public Flux<GetCalendarDto> getCalendar(Instant start, Instant end) {
+		return calendarRepository.findByStartDateTimeBetween(start, end)
+			.flatMap(calendar ->
+				categoryRepository.findByUuid(calendar.getCategoryUuid())
+					.map(category -> GetCalendarDto.builder()
+						.categoryId(calendar.getCategoryUuid())
+						.categoryColor(category.getColor())
+						.title(calendar.getTitle())
+						.startDateTime(calendar.getStartDateTime())
+						.endDateTime(calendar.getEndDateTime())
+						.build()
+					)
 			);
 	}
 }
