d70686251cdb8d5729ef4d875d83eedc59ab735b
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-14 02:51:06 +0900

    test : 변경사항에 맞게 테스트 수정#46

--- src/test/java/com/usememo/jugger/domain/photo/service/PhotoServiceImplementationTest.java
+++ src/test/java/com/usememo/jugger/domain/photo/service/PhotoServiceImplementationTest.java
@@ -1,5 +1,7 @@
 package com.usememo.jugger.domain.photo.service;
 
+import com.usememo.jugger.domain.category.entity.Category;
+import com.usememo.jugger.domain.category.repository.CategoryRepository;
 import com.usememo.jugger.domain.photo.dto.GetPhotoDto;
 import com.usememo.jugger.domain.photo.dto.GetPhotoRequestDto;
 import com.usememo.jugger.domain.photo.entity.Photo;
@@ -7,6 +9,7 @@ import com.usememo.jugger.domain.photo.repository.PhotoRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
@@ -20,21 +23,27 @@ class PhotoServiceImplementationTest {
 	void getPhotoDto_shouldReturnFluxOfGetPhotoDto() {
 		// given
 		PhotoRepository photoRepository = mock(PhotoRepository.class);
-		PhotoServiceImplementation photoService = new PhotoServiceImplementation(photoRepository);
+		CategoryRepository categoryRepository =mock(CategoryRepository.class);
+		PhotoServiceImplementation photoService = new PhotoServiceImplementation(photoRepository,categoryRepository);
 
-		String userUuid = "user1";
+		String userUuid = "123456789a";
 		String categoryUuid = "여행";
+
 		String url1 = "https://s3.amazon.com/photo1.jpg";
 		String url2 = "https://s3.amazon.com/photo2.jpg";
 
 		Photo photo1 = Photo.builder().url(url1).build();
 		Photo photo2 = Photo.builder().url(url2).build();
 
+		String categoryName = "여행";
+		Category mockCategoty = Category.builder().name(categoryName).build();
+
+		when(categoryRepository.findByUuid(categoryUuid)).thenReturn(Mono.just(mockCategoty));
+
 		when(photoRepository.findByUserUuidAndCategoryUuid(userUuid, categoryUuid))
 			.thenReturn(Flux.just(photo1, photo2));
 
 		GetPhotoRequestDto requestDto = GetPhotoRequestDto.builder()
-			.userUuid(userUuid)
 			.categoryUuid(categoryUuid)
 			.build();
 
@@ -43,8 +52,8 @@ class PhotoServiceImplementationTest {
 
 		// then
 		StepVerifier.create(result)
-			.expectNext(GetPhotoDto.builder().url(url1).build())
-			.expectNext(GetPhotoDto.builder().url(url2).build())
+			.expectNext(GetPhotoDto.builder().url(url1).categoryName(categoryName).build())
+			.expectNext(GetPhotoDto.builder().url(url2).categoryName(categoryName).build())
 			.verifyComplete();
 
 		verify(photoRepository, times(1)).findByUserUuidAndCategoryUuid(userUuid, categoryUuid);
