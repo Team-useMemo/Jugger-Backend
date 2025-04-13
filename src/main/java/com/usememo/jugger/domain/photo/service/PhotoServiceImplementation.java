67c6ecf2790df3681544421f8b8a06c23f56afe2
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-14 02:50:55 +0900

    fix : 카테고리를 uuid로 찾아서 카테고리 이름으로 전달하게끔 수정 #46

--- src/main/java/com/usememo/jugger/domain/photo/service/PhotoServiceImplementation.java
+++ src/main/java/com/usememo/jugger/domain/photo/service/PhotoServiceImplementation.java
@@ -2,25 +2,34 @@ package com.usememo.jugger.domain.photo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.usememo.jugger.domain.category.entity.Category;
+import com.usememo.jugger.domain.category.repository.CategoryRepository;
 import com.usememo.jugger.domain.photo.dto.GetPhotoDto;
 import com.usememo.jugger.domain.photo.dto.GetPhotoRequestDto;
 import com.usememo.jugger.domain.photo.repository.PhotoRepository;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class PhotoServiceImplementation implements  PhotoService{
 
 	private final PhotoRepository photoRepository;
+	private final CategoryRepository categoryRepository;
 
-	public Flux<GetPhotoDto> getPhotoDto(GetPhotoRequestDto photoRequestDto){
-		return photoRepository
-			.findByUserUuidAndCategoryUuid(photoRequestDto.getUserUuid(), photoRequestDto.getCategoryUuid())
-			.map(photo -> GetPhotoDto.builder()
-				.url(photo.getUrl())
-				.build());
+	public Flux<GetPhotoDto> getPhotoDto(GetPhotoRequestDto photoRequestDto) {
+		return categoryRepository.findByUuid(photoRequestDto.getCategoryUuid())
+			.flatMapMany(category ->
+				photoRepository
+					.findByUserUuidAndCategoryUuid("123456789a", photoRequestDto.getCategoryUuid())
+					.map(photo -> GetPhotoDto.builder()
+						.url(photo.getUrl())
+						.categoryName(category.getName())
+						.timestamp(photo.getCreatedAt())
+						.build())
+			);
 	}
 
 }
