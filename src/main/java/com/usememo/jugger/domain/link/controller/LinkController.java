be738f6ed3fdd822ef7baf2e2b1364fb6a0f2a61
JjungminLee <julie0964@naver.com>
2025-04-29 22:04:10 +0900

    feat: swagger 추가

--- src/main/java/com/usememo/jugger/domain/link/controller/LinkController.java
+++ src/main/java/com/usememo/jugger/domain/link/controller/LinkController.java
@@ -11,16 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.usememo.jugger.domain.link.dto.GetLinkDto;
 import com.usememo.jugger.domain.link.service.LinkService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Tag(name = "링크 API", description = "링크 API에 대한 설명입니다.")
 @RequestMapping("/api/v1/links")
 @RequiredArgsConstructor
 public class LinkController {
 
 	private final LinkService linkService;
 
+	@Operation(summary = "[GET] 링크 조회")
 	@GetMapping
 	public Mono<ResponseEntity<List<GetLinkDto>>> getLinks(@RequestParam("categoryId") String categoryUuid) {
 		return linkService.getLinks(categoryUuid)
