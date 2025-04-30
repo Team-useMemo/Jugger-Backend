1128ab5e3f97fcb4a0a53620299670ac5c82af2a
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-30 20:42:15 +0900

    feat : jwt 토큰 발급자 생성 #47

---
+++ src/main/java/com/usememo/jugger/global/security/JwtTokenProvider.java
@@ -0,0 +1,77 @@
+package com.usememo.jugger.global.security;
+
+import static io.jsonwebtoken.security.Keys.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.MacAlgorithm;
+import javax.crypto.SecretKey;
+
+
+@Component
+public class JwtTokenProvider {
+	@Value("${spring.jwt.secret}")
+	private String SECRET_KEY;
+
+	private final MacAlgorithm alg = Jwts.SIG.HS512;
+
+	public String createAccessToken(UUID userId) {
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + 3600000);
+
+
+		SecretKey key = hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+		return Jwts.builder()
+			.claims(Map.of("sub",userId.toString() ))
+			.issuedAt(now)
+			.expiration(expiryDate)
+			.signWith(key, alg)
+			.compact();
+	}
+
+	public String createRefreshToken(UUID userId, long validityMillis){
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + validityMillis);
+		SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+		return Jwts.builder()
+			.claims(Map.of("sub",userId.toString()))
+			.issuedAt(now)
+			.expiration(expiryDate)
+			.signWith(key,alg)
+			.compact();
+	}
+
+	public String getUserIdFromToken(String token){
+
+		return Jwts.parser()
+			.verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getSubject();
+	}
+
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser()
+				.verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+				.build()
+				.parseSignedClaims(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
