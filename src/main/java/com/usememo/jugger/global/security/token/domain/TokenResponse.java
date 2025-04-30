03372be78e03588236d9ef2b44f50b9c01b5e677
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-01 03:35:59 +0900

    fix : uuid를 string으로 받게 수정#47

---
+++ src/main/java/com/usememo/jugger/global/security/token/domain/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.usememo.jugger.global.security.token.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+	private String accessToken;
+	private String refreshToken;
+}
