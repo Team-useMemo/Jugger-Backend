cf683e0c50b8aba02301c9ee1ac76764c34a4350
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-01 03:37:44 +0900

    feat : 카카오 로그인 추가 - #47

--- src/main/java/com/usememo/jugger/global/security/token/controller/AuthController.java
+++ src/main/java/com/usememo/jugger/global/security/token/controller/AuthController.java
@@ -1,23 +1,22 @@
 package com.usememo.jugger.global.security.token.controller;
 
-import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usememo.jugger.domain.user.repository.UserRepository;
-import com.usememo.jugger.global.security.CustomOAuth2User;
 import com.usememo.jugger.global.security.JwtTokenProvider;
+import com.usememo.jugger.global.security.token.domain.TokenResponse;
 import com.usememo.jugger.global.security.token.repository.RefreshTokenRepository;
+import com.usememo.jugger.global.security.token.service.KakaoOAuthService;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -29,6 +28,7 @@ public class AuthController {
 	private final RefreshTokenRepository refreshTokenRepository;
 	private final UserRepository userRepository;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final KakaoOAuthService kakaoService;
 
 	@PostMapping(value = "/refresh", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Mono<ResponseEntity<String>> refreshAccessToken(@CookieValue(name = "refresh_token",required = false) String refreshToken){
@@ -43,7 +43,7 @@ public class AuthController {
 					String userId = jwtTokenProvider.getUserIdFromToken(savedToken.getToken());
 					return userRepository.findById(userId)
 						.map(user->{
-							String newAccessToken = jwtTokenProvider.createAccessToken(UUID.fromString(userId));
+							String newAccessToken = jwtTokenProvider.createAccessToken(userId);
 							return ResponseEntity.ok().body("{\"accessToken\":\""+newAccessToken+"\"}");
 						});
 			})
@@ -62,11 +62,6 @@ public class AuthController {
 		} catch (Exception e) {
 			return Mono.just(ResponseEntity.noContent().build());
 		}
-		//프론트에서 인가 코드를 받아서 -> 코드를 서버가 가져서 카카오에 요청을 받아서 처리하는 것이다.
-		//사용자 정보를 저장하는 형식인 것이다.
-		//프론트에 jwt 토큰을 넘겨주면 되는 것이다.
-		//redirection 때문에 문제가 발생하는 것이다.
-		//security 설정해서 이대로 쓰기
 		return refreshTokenRepository.deleteByUserId(UUID.fromString(userId))
 			.thenReturn(ResponseEntity
 				.noContent()
@@ -81,11 +76,10 @@ public class AuthController {
 				.build());
 	}
 
-	// @GetMapping("/success")
-	// public Mono<ResponseEntity<Map<String, String>>> loginSuccess(Authentication authentication) {
-	// 	CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
-	// 	String accessToken = jwtTokenProvider.createAccessToken(UUID.fromString(user.getUserId()));
-	// 	return Mono.just(ResponseEntity.ok(Map.of("accessToken", accessToken)));
-	// }
+	@PostMapping("/kakao")
+	public Mono<ResponseEntity<TokenResponse>> loginByKakao(@RequestParam String code) {
+		return kakaoService.loginWithKakao(code)
+			.map(token -> ResponseEntity.ok().body(token));
+	}
 
 }
