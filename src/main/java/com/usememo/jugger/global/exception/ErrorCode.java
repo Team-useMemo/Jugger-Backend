24f0f3dbd4c259610472f2df4c87f25c7289d675
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-30 20:41:44 +0900

    fix  : errorcode 추가 #47

--- src/main/java/com/usememo/jugger/global/exception/ErrorCode.java
+++ src/main/java/com/usememo/jugger/global/exception/ErrorCode.java
@@ -10,7 +10,10 @@ import lombok.Getter;
 public enum ErrorCode {
 	CATEGORY_ALREADY_EXIST(BAD_REQUEST, 400, "이미 존재하는 카테고리입니다"),
 	CATEGORY_IS_NULL(BAD_REQUEST, 400, "카테고리는 NULL값일 수 없습니다"),
-	FILE_UPLOAD_FAIL(INTERNAL_SERVER_ERROR, 500, "S3 파일 업로드에 실패했습니다.");
+	FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, 500, "S3 파일 업로드에 실패했습니다."),
+	NO_REFRESH_TOKEN(UNAUTHORIZED,400,"리프레시 토큰이 없습니다."),
+	EXPIRED_REFRESH_TOKEN(UNAUTHORIZED,400, "만료된 토큰입니다"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "서버 내부 오류입니다.");
 
 	private final HttpStatus httpStatus;
 	private final int code;
