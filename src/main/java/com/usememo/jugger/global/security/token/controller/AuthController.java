60002ffed673352a549cda19d89a2df0cf81622a
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-19 20:56:49 +0900

    refactor : 로그아웃 부분 수정 (#81)
    * feat : error code 추가 #79
    
    * feat : logout을 위한 DTO들 생성 #79
    
    * fix : 로그아웃과 토큰재발급 부분 수정 #79
    
    * fix : 주석 제거 #79
    
    * fix : 리턴 형식 변경 #79

--- src/main/java/com/usememo/jugger/global/security/token/controller/AuthController.java
+++ src/main/java/com/usememo/jugger/global/security/token/controller/AuthController.java
@@ -13,11 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usememo.jugger.domain.user.repository.UserRepository;
+import com.usememo.jugger.global.exception.BaseException;
 import com.usememo.jugger.global.exception.ErrorCode;
 import com.usememo.jugger.global.exception.KakaoException;
 import com.usememo.jugger.global.security.JwtTokenProvider;
@@ -25,6 +27,10 @@ import com.usememo.jugger.global.security.token.domain.KakaoLoginRequest;
 
 import com.usememo.jugger.global.security.token.domain.KakaoSignUpRequest;
 
+import com.usememo.jugger.global.security.token.domain.LogOutRequest;
+import com.usememo.jugger.global.security.token.domain.LogOutResponse;
+import com.usememo.jugger.global.security.token.domain.NewTokenResponse;
+import com.usememo.jugger.global.security.token.domain.RefreshTokenRequest;
 import com.usememo.jugger.global.security.token.domain.TokenResponse;
 import com.usememo.jugger.global.security.token.repository.RefreshTokenRepository;
 import com.usememo.jugger.global.security.token.service.KakaoOAuthService;
@@ -44,55 +50,26 @@ public class AuthController {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final KakaoOAuthService kakaoService;
 
-	@Operation(summary = "[POST] refresh token 으로 새로운 access token 발급")
-	@PostMapping(value = "/refresh", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Mono<ResponseEntity<String>> refreshAccessToken(
-		@CookieValue(name = "refresh_token", required = false) String refreshToken) {
-		if (refreshToken == null) {
-			return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("리프레시 토큰이 없습니다"));
+	@Operation(summary = "[POST] refresh token으로 새로운 access token 발급")
+	@PostMapping(value = "/refresh")
+	public Mono<ResponseEntity<NewTokenResponse>> refreshAccessToken(@RequestBody RefreshTokenRequest request) {
+		String refreshToken = request.refreshToken();
+
+		if (refreshToken == null || refreshToken.isBlank()) {
+			throw new BaseException(ErrorCode.NO_REFRESH_TOKEN);
 		}
-		return refreshTokenRepository.findByToken(refreshToken)
-			.flatMap(savedToken -> {
-				if (!jwtTokenProvider.validateToken(savedToken.getToken())) {
-					return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("만료된 리프레시 토큰입니다."));
-				}
-				String userId = jwtTokenProvider.getUserIdFromToken(savedToken.getToken());
-				return userRepository.findById(userId)
-					.map(user -> {
-						String newAccessToken = jwtTokenProvider.createAccessToken(userId);
-						return ResponseEntity.ok().body("{\"accessToken\":\"" + newAccessToken + "\"}");
-					});
-			})
-			.switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("존재하지 않는 리프레시 토큰입니다.")));
+		return kakaoService.giveNewToken(refreshToken);
 	}
+
 
 	@Operation(summary = "[POST] 로그아웃")
 	@PostMapping("/logout")
-	public Mono<ResponseEntity<Void>> logout(
-		@CookieValue(name = "refresh_token", required = false) String refreshToken) {
-		if (refreshToken == null) {
-			return Mono.just(ResponseEntity.noContent().build());
-		}
+	public Mono<ResponseEntity<LogOutResponse>> logout(@RequestBody LogOutRequest request) {
 
-		String userId;
-		try {
-			userId = jwtTokenProvider.getUserIdFromToken(refreshToken);
-		} catch (Exception e) {
-			return Mono.just(ResponseEntity.noContent().build());
-		}
-		return refreshTokenRepository.deleteByUserId(UUID.fromString(userId))
-			.thenReturn(ResponseEntity
-				.noContent()
-				.header("Set-Cookie", ResponseCookie.from("refresh_token", "")
-					.httpOnly(true)
-					.secure(true)
-					.path("/")
-					.maxAge(0)
-					.build()
-					.toString()
-				)
-				.build());
+		return kakaoService.userLogOut(request.refreshToken())
+			.thenReturn(ResponseEntity.ok().body(new LogOutResponse("로그아웃이 성공적으로 되었습니다.")));
 	}
+
 
 	@Operation(summary = "[POST] 카카오 로그인")
 	@PostMapping("/kakao")
