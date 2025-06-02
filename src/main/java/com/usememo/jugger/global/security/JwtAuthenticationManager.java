b44db6c44b0731839f13bbc071034489ff79763b
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-02 20:03:55 +0900

    feat : master 토큰이랑 id의 경우 인가해주는 분기처리 (#94)
    * feat : master 토큰이랑 id의 경우 인가해주는 분기처리 #88
    
    * feat : master 토큰에 대한 deploy.yml추가 #88

--- src/main/java/com/usememo/jugger/global/security/JwtAuthenticationManager.java
+++ src/main/java/com/usememo/jugger/global/security/JwtAuthenticationManager.java
@@ -1,6 +1,11 @@
 package com.usememo.jugger.global.security;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +16,12 @@ public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
 
 	private final JwtTokenProvider jwtTokenProvider;
 
+	@Value("${master.id}")
+	private String masterUuid;
+
+	@Value("${master.token}")
+	private String masterToken;
+
 	public JwtAuthenticationManager(JwtTokenProvider jwtTokenProvider) {
 		this.jwtTokenProvider = jwtTokenProvider;
 	}
@@ -18,6 +29,18 @@ public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
 	@Override
 	public Mono<Authentication> authenticate(Authentication authentication) {
 		String token = authentication.getCredentials().toString();
+		if (isMasterToken(token)) {
+			CustomOAuth2User principal = new CustomOAuth2User(
+				Map.of("userId", masterUuid), masterUuid
+			);
+			Authentication auth = new UsernamePasswordAuthenticationToken(principal, token, List.of());
+			return Mono.just(auth);
+		}
+
 		return jwtTokenProvider.getAuthentication(token);
+	}
+
+	private boolean isMasterToken(String token){
+		return token.equals(masterToken);
 	}
 }
