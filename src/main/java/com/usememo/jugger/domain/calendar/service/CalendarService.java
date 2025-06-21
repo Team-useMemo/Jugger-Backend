16845e59f41f55f94d8171fa70931f5c5f86c4b4
JjungminLee <julie0964@naver.com>
2025-06-21 19:58:37 +0900

    feat: 일정조회, 일정 추가 api uuid 하드코딩 삭제

--- src/main/java/com/usememo/jugger/domain/calendar/service/CalendarService.java
+++ src/main/java/com/usememo/jugger/domain/calendar/service/CalendarService.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import com.usememo.jugger.domain.calendar.dto.GetCalendarDto;
 import com.usememo.jugger.domain.calendar.dto.PostCalendarDto;
 import com.usememo.jugger.domain.calendar.entity.Calendar;
+import com.usememo.jugger.global.security.CustomOAuth2User;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,7 +13,7 @@ import reactor.core.publisher.Mono;
 public interface CalendarService {
 
 	// NOTE: V1용 API
-	Mono<Calendar> postCalendar(PostCalendarDto postCalendarDto);
+	Mono<Calendar> postCalendar(PostCalendarDto postCalendarDto, CustomOAuth2User customOAuth2User);
 
-	Flux<GetCalendarDto> getCalendar(Instant start, Instant end);
+	Flux<GetCalendarDto> getCalendar(Instant start, Instant end, CustomOAuth2User customOAuth2User);
 }
