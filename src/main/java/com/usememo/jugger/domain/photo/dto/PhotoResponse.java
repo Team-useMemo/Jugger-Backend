e03f4f525e821dedee21b4de1be1d26e60f2be24
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-26 02:42:04 +0900

    style : dto 명칭 변경 #126

--- src/main/java/com/usememo/jugger/domain/photo/dto/PhotoResponse.java
+++ src/main/java/com/usememo/jugger/domain/photo/dto/PhotoResponse.java
@@ -1,20 +1,15 @@
 package com.usememo.jugger.domain.photo.dto;
 
 import java.time.Instant;
-import java.util.List;
-
-import com.usememo.jugger.domain.photo.entity.Photo;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class GetPhotoDto {
+public class PhotoResponse {
 	private String url;
 	private String categoryId;
 	private String description;
 	private Instant timestamp;
-
-
 }
