d17bc35f7f8dc20df3ce57ef3f52feac171c2c71
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-28 02:55:30 +0900

    style : 링크 response의 categoryId로 변경 #136

--- src/main/java/com/usememo/jugger/domain/link/dto/GetLinkDto.java
+++ src/main/java/com/usememo/jugger/domain/link/dto/GetLinkDto.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 @Builder
 public class GetLinkDto {
-	private String categoryUuid;
+	private String categoryId;
 	private String categoryName;
 
 	private List<LinkData> linkData;
