67ca6871c6f86f086f4dd1d740fd4527a978713a
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-28 21:50:17 +0900

    fix : created_at 생기게 변경 #136

--- src/main/java/com/usememo/jugger/domain/link/entity/Link.java
+++ src/main/java/com/usememo/jugger/domain/link/entity/Link.java
@@ -1,29 +1,46 @@
 package com.usememo.jugger.domain.link.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.usememo.jugger.global.utils.BaseTimeEntity;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Document(collection = "links")
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
-public class Link {
+public class Link extends BaseTimeEntity implements Persistable<String> {
 	@Id
 	private String uuid;
 	private String userUuid;
 	private String chatUuid;
 
+	@Setter
 	private String url;
+	@Setter
 	private String categoryUuid;
 
 	private String caption;
 
+
 	public Link() {
+	}
+
+	@Override
+	public String getId(){
+		return getUuid();
+	}
+
+	@Override
+	public boolean isNew() {
+		return getCreatedAt() == null;
 	}
 }
 
