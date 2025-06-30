8c0cc32ba75c12b5563e1612e3a2aedd6352de44
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-30 15:42:32 +0900

    feat : 사진 설명 수정 구현 #140

--- src/main/java/com/usememo/jugger/domain/photo/service/PhotoService.java
+++ src/main/java/com/usememo/jugger/domain/photo/service/PhotoService.java
@@ -4,9 +4,12 @@ import java.time.Instant;
 
 import com.usememo.jugger.domain.photo.dto.PhotoResponse;
 import com.usememo.jugger.domain.photo.dto.GetPhotoRequestDto;
+import com.usememo.jugger.domain.photo.dto.PhotoUpdateRequest;
+import com.usememo.jugger.global.response.BaseResponse;
 import com.usememo.jugger.global.security.CustomOAuth2User;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface PhotoService {
 	Flux<PhotoResponse> getPhotoDto(GetPhotoRequestDto photoRequestDto, CustomOAuth2User customOAuth2User);
@@ -15,4 +18,5 @@ public interface PhotoService {
 
 	Flux<PhotoResponse> getPhotoCategoryAndDuration(String categoryId, Instant before, int page, int size, CustomOAuth2User customOAuth2User);
 
+	Mono<BaseResponse> updatePhotoDescription(CustomOAuth2User customOAuth2User, PhotoUpdateRequest request);
 }
