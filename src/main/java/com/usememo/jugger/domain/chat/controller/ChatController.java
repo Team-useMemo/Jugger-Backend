cedcc89f84ced5926c37c16a2b18daddc4050e65
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-07-07 23:15:14 -0700

    style : 주석 제거 #146

--- src/main/java/com/usememo/jugger/domain/chat/controller/ChatController.java
+++ src/main/java/com/usememo/jugger/domain/chat/controller/ChatController.java
@@ -107,7 +107,6 @@ public class ChatController {
 	@Operation(summary = "[PATCH] 채팅 카테고리 변경",description = "채팅의 카테고리를 변경하는데 사용하는 메소드입니다.")
 	@PatchMapping("/category")
 	public Mono<ResponseEntity<ChatResponse>> patchChatCategory(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestParam String chatId, @RequestParam String newCategoryId){
-		//여기도 전체적으로 채팅으로 들어가서 repository로 id를 들어가서 교체하는 과정이 들어가야 한다.
 		return chatService.changeCategory(customOAuth2User, chatId, newCategoryId)
 			.thenReturn(ResponseEntity.ok().body(new ChatResponse(200,"카테고리가 변경되었습니다.")));
 	}
