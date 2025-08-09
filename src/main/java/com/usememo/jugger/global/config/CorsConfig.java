51f406ff53316f624ebef036848e1313fb7dac29
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-08-09 16:59:01 +0900

    hotfix : cors 추가

--- src/main/java/com/usememo/jugger/global/config/CorsConfig.java
+++ src/main/java/com/usememo/jugger/global/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(List.of(
 			"https://jugger.netlify.app",
+			"https://jugger.usememo.site/",
 			"http://localhost:5173",
 			"http://localhost:3000",
 			"http://localhost:5174",
